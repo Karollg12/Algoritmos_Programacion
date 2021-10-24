0b00a79443da1671268a4c429853824d70928b0a
Karoll Stefany Gómez Triana <91572492+Karollg12@users.noreply.github.com>
2021-10-24 12:37:25 -0500

    Create Ejercicio_2.py

---
+++ Estructura de control selectivo_Algoritmo/Ejercicio_2.py
@@ -0,0 +1,13 @@
+"""
+Entradas 
+Salariobruto -> float -> sb 
+Salidas 
+Salarioneto -> flotar -> sn  
+"""
+sb = float (input("Digitar salario bruto:"))
+si = (sb 900000)
+ cn = sb + (sb*0,15)
+ print ("Salario neto es igual:"+ str (cn))
+ else:
+  cn =sb + (sb+0,12)
+  Print("El salario neto es igual:" + str (cn))
