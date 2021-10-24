c584f5add153c7755c19b6699942533440509f2a
Karoll Stefany Gómez Triana <91572492+Karollg12@users.noreply.github.com>
2021-10-24 12:37:11 -0500

    Create Ejercicio_6.py

---
+++ Estructura de control selectivo_Algoritmo/Ejercicio_6.py
@@ -0,0 +1,28 @@
+"""
+Entradas 
+variable1 --> int A 
+variable2 --> int B 
+variable3 --> int C 
+variable4 --> int D 
+salidas 
+miles --> int --> m 
+decenas --> int --> d 
+numero --> int --> n 
+"""
+A = int (input ( "Digite el valor de la variable A:" ))
+B = int (input ( "Digite el valor de la variable B:" ))
+C = int (input ( "Digite el valor de la variable C:" ))
+D = int (input ( "Digite el valor de la variable D:" ))
+
+if ( c > = 5 ):
+   m = A * 1000 
+   d = B * 100 
+   n = m + (d + 100)
+   print (n)
+else: 
+   m = A * 1000
+   ds = B * 100
+   n = m + ( d + 100 )
+   print (n)
+
+
